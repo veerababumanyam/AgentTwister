775e838eecc5db90b0c8f9db3addb850cb535de8
Veera Babu Manyam <veera@sawas.org>
2026-03-06 13:34:39 +0100

    chore: strictly enforce .gitignore and purge build artifacts from index

--- frontend/src/lib/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Library Module
- *
- * Central export point for all utility functions and clients.
- */
-
-export * from "./api";
-export * from "./utils";
-export { default as api } from "./api";
